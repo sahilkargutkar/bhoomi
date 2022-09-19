b9e6d5aeeab2ce30f222652236de9a8d969526c7
Sahil Kargutkar <sahilkargutkar.sk@gmail.com>
2022-09-19 17:05:53 +0530

    make it better

--- bhoomi-backend/server.js
+++ bhoomi-backend/server.js
@@ -1,10 +1,20 @@
 const dotenv = require("dotenv");
 const app = require("./app");
 const connectDatabase = require("./config/database");
+const path = require("path");
 
 dotenv.config({ path: "bhoomi-backend/config/config.env" });
 
 connectDatabase();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join("bhoomi-client/build")));
+  app.get("*", (req, res) => {
+    res.sendfile(
+      path.resolve(__dirname, "bhoomi-client", "build", "index.html")
+    );
+  });
+}
 
 app.listen(process.env.PORT, () => {
   console.log(`Server Working on Port http://localhost:${process.env.PORT}`);
